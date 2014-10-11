603ee3b712edab98cea07029c87a85e85948140d
Edwin Finch <edwinfinch8@gmail.com>
2014-10-11 16:50:54 -0400

    Revamp with new link option, and other small spacing improvements

--- upload_file.php
+++ upload_file.php
@@ -1,21 +1,22 @@
 <?php
 
-$destination_path = $_REQUEST["destination"] . "/";
+$destination_path = $_REQUEST["destination"] . "/"; 
 $target_path = "/var/www/" . $destination_path;
 $target_path = $target_path . basename( $_FILES['uploadedfile']['name']);
+$fix_path = $destination_path . basename( $_FILES['uploadedfile']['name']);
 
-echo "User=" .          $_ENV[USER] . "<br />";
-echo "Source=" .        $_FILES['uploadedfile']['name'] . "<br />";
-echo "Destination=" .   $destination_path . "<br />";
-echo "Target path=" .   $target_path . "<br />";
-echo "Size=" .          $_FILES['uploadedfile']['size'] . "<br />";
-//echo "Tmp name=" .    $_FILES['uploadedfile']['tmp_name'] . "<br />"; 
+echo "Source = " .        $_FILES['uploadedfile']['name'] . "<br />"; 
+echo "Destination = " .   $destination_path . "<br />"; 
+echo "Target path = " .   $target_path . "<br />"; 
+echo "Size (bytes) = " .          $_FILES['uploadedfile']['size'] . "<br />"; 
 
 
 if(move_uploaded_file($_FILES['uploadedfile']['tmp_name'], $target_path)) {
-    echo "The file ".  basename( $_FILES['uploadedfile']['name']).
-    " has been uploaded";
+    echo "File: ".  basename( $_FILES['uploadedfile']['name']). 
+    " uploaded successfully.<br><br>";
+	$path_no_spaces = str_replace(' ', '%20', $fix_path);
+	echo "<a style=\"font-family:'Roboto';padding:10px;\" href=/" . $path_no_spaces . '>File link</a>';
 } else{
-    echo "There was an error uploading the file, please try again!";
+    echo "An error occurred while uploading the file.";
 }
 ?>
