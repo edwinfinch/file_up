3bd2c2083840255c6d626b130c2bb3c27bf59312
Edwin Finch <edwinfinch8@gmail.com>
2014-10-11 16:50:23 -0400

    2.0! New look, username, and more.

--- index.php
+++ index.php
@@ -1,69 +1,83 @@
 <?php
-/************************************************************************/
-/* PHP Simple PasswordProtect v1.0                                      */
-/* ===========================                                          */
-/*                                                                      */
-/*   Written by Steve Dawson - http://www.stevedawson.com               */
-/*   Freelance Web Developer - PHP, MySQL, HTML programming             */
-/*                                                                      */
-/* This program is free software. You can redistribute it and/or modify */
-/* but please leave this header intact, thanks                          */
-/************************************************************************/
-##########################################################################
-$password = "password";  // Modify Password to suit for access, Max 10 Char.
-##########################################################################
+$password = "password";
+$username = "username";
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
 <head>
-<title>Password</title>
+<title>File Upload</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<style type="text/css">
-<!--
-P { FONT-SIZE: 8pt; COLOR: #000000; FONT-FAMILY: Verdana, Tahoma, Arial}
-TD { FONT-SIZE: 8pt; COLOR: #000000; FONT-FAMILY: Verdana, Tahoma, Arial}
--->
-</style>
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.5.0/pure-min.css">
+<link href='http://fonts.googleapis.com/css?family=Roboto:300' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/buttons.css">
+<link rel="stylesheet" href="css/buttons-core.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<script>
+   function chooseFile() {
+      document.getElementById("uploadInput").click();
+   }
+</script>
+
+<script>
+   function changeName() {
+      var x = document.getElementById("uploadInput").value;
+	  
+	  if(x === ''){
+		x = "No file!";
+	  }
+	  
+	  document.getElementById("fileDisplay").innerHTML = x;
+   }
+</script>
+
 </head>
 <body>
-<?php
+<?php 
 // If password is valid let the user get access
-if (isset($_POST["password"]) && ($_POST["password"]=="$password")) {
+if (isset($_POST["password"]) && ($_POST["password"]=="$password") && ($_POST["username"]=="$username")) {
 ?>
-
 <!-- START OF HIDDEN HTML - PLACE YOUR CONTENT HERE -->
 
 <html>
 <head></head>
 <body>
-<h4> File uploads </h4>
+<div align="center">
 <form enctype="multipart/form-data" action="upload_file.php" 
-        method="post">
+	method="post">
 <p>
-Select File:
-<input type="file" size="35" name="uploadedfile" />
-<br />
+<h2 style="font-family:'Roboto';padding:10px;">File</h2>
+<!--<input type="file" size="35" name="uploadedfile" class="pure-button"/>-->
+<div style="height:0px;overflow:hidden">
+   <input type="file" size="35" id="uploadInput" name="uploadedfile" onchange="changeName();"/>
+</div>
+<button type="button" onclick="chooseFile();" class="pure-button" id="fileDisplay">Choose File</button>
+<br>
+<h4 id="fileChosen"></h4>
 
-Destination
+<h2 style="font-family:'Roboto';padding:10px;">Destination</h2>
 <input type="text" size="35" name="destination" 
        value= "<?php echo $_ENV["USER"];?>" />
-<br />
-<input type="submit" name="Upload" value="Upload" />
+<br><br>
+<input type="submit" name="Upload" value="Upload" class="pure-button pure-button-primary"/>
 </p>
 </form>
+</div>
 </body>
 </html>
 
 <!-- END OF HIDDEN HTML -->
-<?php
+<?php 
 }
 else
 {
 // Wrong password or no password entered display this message
 if (isset($_POST['password']) || $password == "") {
-  print "<p align=\"center\"><font color=\"red\"><b>Incorrect Password</b><br>Please enter the correct password</font></p>";}
-  print "<form method=\"post\"><p align=\"center\">Please enter your password for access<br>";
-  print "<input name=\"password\" type=\"password\" size=\"25\" maxlength=\"10\"><input value=\"Login\" type=\"submit\"></p></form>";
+  print "<p align=\"center\" style=\"font-family:'Roboto';padding:10px;\"><font color=\"red\"><b>Incorrect.</b><br>Re-enter password/username combo.</font></p>";}
+  print "<form method=\"post\"><p style=\"font-family:'Roboto';padding:10px;\">Username:<br>";
+  print "<input name=\"username\" type=\"text\" size=\"25\" maxlength=\"10\">";
+  print "<p style=\"font-family:'Roboto';padding:10px;\">Password:<br>";
+  print "<input name=\"password\" type=\"password\" size=\"25\" maxlength=\"10\"><br><br><input value=\"Login\" type=\"submit\" class=\"pure-button pure-button-primary\"></p></p></form>";
 }
 ?>
 <BR>
